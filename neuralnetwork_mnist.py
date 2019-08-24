0d7f7a5812de8c816d956f1c4035163f1075ff5b
mpark2021 <mpark2021@gmail.com>
2019-08-24 20:49:16 +0900

    Neural Network Run

--- neuralnetwork_mnist.py
+++ neuralnetwork_mnist.py
@@ -21,6 +21,7 @@ model = tf.keras.models.Sequential(
     [
         tf.keras.layers.Flatten(input_shape=(28, 28)),
         tf.keras.layers.Dense(25,activation="sigmoid"),
+        tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(10,activation="sigmoid")
     ]
 )
@@ -30,3 +31,10 @@ model.compile(optimizer="adam",
               metrics=["accuracy"])
 
 model.summary()
+
+model.fit(x_train, y_train, epochs=20)
+
+result = model.evaluate(x_train, y_train)
+print(result)
+result = model.evaluate(x_test, y_test)
+print(result)
